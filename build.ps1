6081ad99f503334c45cfcca3624b1a3c3a907466
James Dawson <james.dawson@endjin.com>
2021-09-22 18:29:43 +0100

    More debugging

--- build.ps1
+++ build.ps1
@@ -121,7 +121,7 @@ $GitVersionToolVersion = "5.7.0"
 task . FullBuild
 
 # extensibility tasks
-task PreBuild { tree }
+task PreBuild { pwd; tree }
 task PostBuild {}
 task PreTest {}
 task PostTest {}

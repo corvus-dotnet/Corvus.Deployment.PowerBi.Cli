09eb3617d4ad37dbb28b133d226e856dab7d93f1
dependjinbot <dependjinbot@endjin.com>
2024-05-14 02:15:28 +0000

    Updated one or more module dependencies in build.ps1

--- build.ps1
+++ build.ps1
@@ -76,7 +76,7 @@ param (
     [string] $BuildModulePath,
 
     [Parameter()]
-    [version] $BuildModuleVersion = "1.5.5",
+    [version] $BuildModuleVersion = "1.5.6",
 
     [Parameter()]
     [version] $InvokeBuildModuleVersion = "5.10.3"
